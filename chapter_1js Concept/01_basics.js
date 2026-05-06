9a04bfc157946cc5480150ef73770070bff522a4
aanya <aanya@cyberpointmedia.com>
2026-05-06 19:37:21 +0530

    1st commit

---
+++ chapter_1js Concept/01_basics.js
@@ -0,0 +1,4 @@
+console.log(" hello The Testing Academy")
+
+let x = 10;
+console.log(x);
---
+++ chapter_1js Concept/02_js_verifysetup.js
@@ -0,0 +1,8 @@
+console.log(process.platform);
+
+//Darvin for MAc
+//win32 for windows
+//linux for linux
+
+
+console.log(process.arch);
