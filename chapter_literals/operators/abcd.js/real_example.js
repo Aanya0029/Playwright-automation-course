3c744e7225ffbd7c86fd8349c5476bbadd2b2edf
Aanya Vats <111377925+Aanya0029@users.noreply.github.com>
2026-06-03 12:01:15 +0530

    commit

---
+++ chapter_literals/operators/abcd.js/real_example.js
@@ -0,0 +1,9 @@
+let username = "aanya";
+let password = "12345";
+let isAccountLocked = true;
+
+if ((username === "aanya" && password === "12345") && isAccountLocked) {
+    console.log("Login successful!");
+} else {
+    console.log("Login failed!");
+}
---
+++ chapter_literals/operators/abcd.js/task.js
@@ -0,0 +1,10 @@
+let no = 7;
+
+if (no % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");}
+
+
+
+    
